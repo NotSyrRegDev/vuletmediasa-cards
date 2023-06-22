88989e96d63eaf1c03cb64b7c053345a1748be71
AmmarGeeks <ammar20021gamer@gmail.com>
2023-06-22 14:04:26 +0300

    news working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import {   Twitter , Instagram  , YouTube , LinkedIn  , WhatsApp  } from '@material-ui/icons';
+// import {   Twitter , Instagram  , YouTube , LinkedIn  , WhatsApp  } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -15,9 +15,9 @@ const Navbar = () => {
 <img src="/images/icons/logo-transparent.png" alt="" className="icon_mid" />
         <div className="mt-1 d-flex-c icons_section">
 
-    <Twitter className='grounded-radiants' onClick={() => openInNewTab('https://twitter.com/vuletmedia')} />
+    {/* <Twitter className='grounded-radiants' onClick={() => openInNewTab('https://twitter.com/vuletmedia')} />
 
-        <Instagram onClick={() => openInNewTab('https://www.instagram.com/vuletmedia.sa/')} />
+        <Instagram onClick={() => openInNewTab('https://www.instagram.com/vuletmedia.sa/')} /> */}
         
         
         
